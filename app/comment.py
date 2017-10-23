19ed6ca7f02b04b2770d87a11014168575c11c22
Grant Gapinski <grant-gapinski@bethel.edu>
2017-10-23 12:13:18 -0500

    Added stuff

--- app/comment.py
+++ app/comment.py
@@ -5,3 +5,8 @@ from app import app
 @app.route('/comments')
 def comments():
     return render_template('comments.html')
+
+
+@app.route('/comments/new')
+def create_comment():
+    return render_template('newCommentForm.html')
