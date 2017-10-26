e3d88288b90d65ec10f78ea85770850a7f4ceab3
Grant Gapinski <grant-gapinski@bethel.edu>
2017-10-26 14:00:37 -0500

    Fixing up comment object

--- app/comment.py
+++ app/comment.py
@@ -4,9 +4,35 @@ from app import app
 
 @app.route('/comments')
 def comments():
+    # TODO Load comments once feature is added
     return render_template('comments.html')
 
 
 @app.route('/comments/new')
-def create_comment():
+def new_comment():
     return render_template('newCommentForm.html')
+
+
+def delete_comment():
+    pass
+
+
+# Create Comment Object
+class Comment:
+
+    def __init__(self, name, post, time_stamp):
+        self.name = name
+        self.post = post
+        self.time_stamp = time_stamp
+        self.identifier = None
+        
+    def get_post(self):
+        return self.post
+
+    def get_name(self):
+        return self.name
+
+    def set_identifier(self):
+        #Generates identifier
+
+        self.identifier = 124
