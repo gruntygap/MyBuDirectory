fcef76180419c3bfdd092fd4e4f65f4a476c5248
Grant Gapinski <grant-gapinski@bethel.edu>
2017-10-31 20:37:10 -0500

    Accesses the Data

--- app/comment.py
+++ app/comment.py
@@ -8,9 +8,10 @@ from sqlite3 import Error
 
 
 @app.route('/comments')
-def comments():
+def start_comments():
     # TODO Load comments once feature is added
-    return render_template('comments.html')
+    comments = get_comments()
+    return render_template('comments.html', comments=comments)
 
 
 @app.route('/comments/new')
@@ -22,7 +23,6 @@ def new_comment():
 
 def create_comment(author, content):
     comment = Comment(author, content)
-    print comment.identifier
     upload_comment(comment)
 
 
@@ -45,6 +45,7 @@ def upload_comment(comment):
     # Saves
     conn.commit()
 
+
 @app.route('/comments/delete/<identifier>')
 def delete_comment(identifier):
     conn = sqlite3.connect(config.database_path)
@@ -56,14 +57,32 @@ def delete_comment(identifier):
         return "Could not delete %s" % e
 
 
+def get_comments():
+    comments = []
+    results = []
+    conn = sqlite3.connect(config.database_path)
+    c = conn.cursor()
+    try:
+        c.execute("SELECT * FROM comments")
+        results = c.fetchall()
+
+    except Error as e:
+        print "Error: %s" % e
+
+    for result in results:
+        comments.append(Comment(result[1], result[2], result[3], result[0]))
+
+    return comments
+
+
 # Comment Object
 class Comment:
 
-    def __init__(self, author, content):
+    def __init__(self, author, content, time_stamp=None, identifier=None):
         self.author = author
         self.content = content
-        self.time_stamp = self.set_time_stamp()
-        self.identifier = self.set_identifier()
+        self.time_stamp = self.set_time_stamp(time_stamp)
+        self.identifier = self.set_identifier(identifier)
 
     def get_content(self):
         return self.content
@@ -72,15 +91,22 @@ class Comment:
         return self.author
 
     @staticmethod
-    def set_time_stamp():
-        # creates time stamp of creation of the comment
-        time = datetime.datetime.now().isoformat()
-        return time
+    def set_time_stamp(time_stamp):
+        if time_stamp is None:
+            # creates time stamp of creation of the comment
+            time = datetime.datetime.now().isoformat()
+            return time
+        else:
+            return time_stamp
+
 
     @staticmethod
-    def set_identifier():
-        # Generates identifier - takes the first 8 characters
-        # TODO May create collisions, when that happens, will have to replace method
-        # http://hashids.org/python/
-        identifier = str(uuid.uuid4())[:8]
-        return identifier
+    def set_identifier(identifier):
+        if identifier is None:
+            # Generates identifier - takes the first 8 characters
+            # TODO May create collisions, when that happens, will have to replace method
+            # http://hashids.org/python/
+            identifier = str(uuid.uuid4())[:8]
+            return identifier
+        else:
+            return identifier
