6a742f09d72cd9615725d27f41fc9b46f85a3abc
Grant Gapinski <grant-gapinski@bethel.edu>
2017-11-02 14:06:27 -0500

    Finished Barebones Create User

--- app/user.py
+++ app/user.py
@@ -10,9 +10,7 @@ from sqlite3 import Error
 @app.route('/user/new', methods=['POST'])
 def new_user():
     data = request.form
-    print data['username']
-    print data['email']
-    print data['pass']
+    create_user(data['username'], data['email'], data['pass'])
     return "Good"
 
 
@@ -21,7 +19,14 @@ def check_user():
     pass
 
 
-def create_user():
+def create_user(username, email, password):
+    new_person = User(username, email, password)
+    print new_person.username
+    print new_person.email
+    print new_person.password
+    print new_person.identifier
+    print new_person.day_created
+    print new_person.last_visit
     pass
 
 
@@ -39,11 +44,37 @@ def activate_user():
 
 class User:
 
-    def __init__(self, username, email, password, day_created, last_visit, activated, identifier):
+    def __init__(self, username, email, password, identifier=None, day_created=None, last_visit=None):
         self.username = username
         self.email = email
         self.password = password
-        self.day_created = day_created
-        self.last_visit = last_visit
-        self.activated = activated
-        self.identifier = identifier
+        self.day_created = self.set_time_stamp(day_created)
+        self.last_visit = self.set_last_visit(last_visit)
+        self.identifier = self.set_identifier(identifier)
+
+    @staticmethod
+    def set_time_stamp(time_stamp):
+        if time_stamp is None:
+            # creates time stamp of creation of the comment
+            time = datetime.datetime.now().strftime("%m/%d/%y - %H:%M")
+
+            return time
+        else:
+            return time_stamp
+
+    @staticmethod
+    def set_last_visit(last_visit):
+        if last_visit is None:
+            time = datetime.datetime.now().strftime("%m/%d/%y - %H:%M")
+        return time
+
+    @staticmethod
+    def set_identifier(identifier):
+        if identifier is None:
+            # Generates identifier - takes the first 8 characters
+            # TODO May create collisions, when that happens, will have to replace method
+            # http://hashids.org/python/
+            identifier = str(uuid.uuid4())[:8]
+            return identifier
+        else:
+            return identifier
